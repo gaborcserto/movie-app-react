722c5c5e91d346e6252e69bd6586059e66a211b2
Gabor Cserto <csertog@gmail.com>
2020-07-11 01:39:29 +0200

    trying the react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar fixed="top" sticky="top" bg="transparent" expand="lg" className="MainNav">
+        <Navbar.Brand href="#home">Movie Search</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <header className="masthead">
+        <div className="container">
+          <div className="masthead-subheading">Welcome</div>
+          <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
