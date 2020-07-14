2af1d2de9f910e1de941e8e2fbe6235208603170
Gabor Cserto <csertog@gmail.com>
2020-07-14 15:50:57 +0200

    implement material ui

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
 
 const Search = (props) => {
 	const [searchValue, setSearchValue] = useState("Batman");
@@ -24,15 +25,24 @@ const Search = (props) => {
 	}
 
 	return (
-		<form className="Search__form" noValidate autoComplete="off">
-			<TextField id="standard-basic" label="Search"
-			           value={searchValue}
-			           onChange={handleSearchInputChanges}
-			           type="text"/>
-			<IconButton aria-label="delete" onClick={callSearchFunction} >
-				<SearchIcon fontSize="large" />
-			</IconButton>
-		</form>
+		<Paper className="Search__wrapper" elevation={3}>
+			<form className="Search__form" noValidate autoComplete="off">
+				<TextField
+					className="Search__form__input"
+					id="standard-basic"
+					label="Search"
+					value={searchValue}
+				    onChange={handleSearchInputChanges}
+				    type="text"/>
+				<IconButton
+					onClick={callSearchFunction}
+					className="Search__form__button"
+					color="primary"
+					aria-label="search">
+					<SearchIcon />
+				</IconButton>
+			</form>
+		</Paper>
 	);
 }
 
