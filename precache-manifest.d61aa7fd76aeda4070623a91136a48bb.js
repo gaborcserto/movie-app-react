599be1f8e2c6296db25eb70fb76004f995d63e27
Gabor Cserto <csertog@gmail.com>
2020-07-28 15:08:53 +0200

    Updates

---
+++ precache-manifest.d61aa7fd76aeda4070623a91136a48bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736a32483b418aec36b1149862945c49",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "776322143ad1e12dc237",
+    "url": "/movie-app-react/static/css/main.7b4de6b7.chunk.css"
+  },
+  {
+    "revision": "71b8aa4e2e9f794e8cd9",
+    "url": "/movie-app-react/static/js/2.c9b77531.chunk.js"
+  },
+  {
+    "revision": "09774ec3b41195d553eb76ed8ce300f5",
+    "url": "/movie-app-react/static/js/2.c9b77531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776322143ad1e12dc237",
+    "url": "/movie-app-react/static/js/main.28c254db.chunk.js"
+  },
+  {
+    "revision": "64e0037c206578654b3b",
+    "url": "/movie-app-react/static/js/runtime-main.c5c16297.js"
+  },
+  {
+    "revision": "9838b26ec8087350d0d6179c92feb6e0",
+    "url": "/movie-app-react/static/media/hero.9838b26e.jpg"
+  }
+]);
