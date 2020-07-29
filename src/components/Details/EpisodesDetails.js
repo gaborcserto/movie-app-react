9b7fcdc3c7393ef1339a6216726abbbfeb2b305b
Gabor Cserto <csertog@gmail.com>
2020-07-29 02:27:50 +0200

    Added episodes date

--- src/components/Details/EpisodesDetails.js
+++ src/components/Details/EpisodesDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Img from "../ImgLoader";
+import moment from "moment";
 
 const episodesDetails = props => {
 	console.log(props)
@@ -28,6 +29,9 @@ const episodesDetails = props => {
 					<p className="episodes__items__detail__number badge badge-pill badge-dark">
 						{`S${episode.season_number}, Ep${episode.episode_number}`}
 					</p>
+					<p className="episodes__items__detail__date">
+						{episode.air_date ? moment(episode.air_date).format('MMM D, YYYY') : 'N/A'}
+					</p>
 					<div className="episodes__items__detail__overview">{episode.overview}</div>
 				</Col>
 			</Row>
@@ -35,6 +39,7 @@ const episodesDetails = props => {
 
 	return (
 		<div className="episodes">
+			<h2>{props.title}</h2>
 			{episodesItems}
 		</div>
 	)
