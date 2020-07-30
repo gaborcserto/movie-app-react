92c4fb05181cdf3da1e449a510034945e8b13c25
Gabor Cserto <csertog@gmail.com>
2020-07-30 13:29:37 +0200

    Updates

--- precache-manifest.9b1436bce45fa45074c40df2572ba23e.js
+++ precache-manifest.9b1436bce45fa45074c40df2572ba23e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0add4d0e786c18fca7fede9e877bcf48",
+    "revision": "83a6257e08d78992ea2e5b3135c67e4e",
     "url": "/movie-app-react/index.html"
   },
   {
-    "revision": "d05999c5b07fa8aaf733",
-    "url": "/movie-app-react/static/css/main.a3b7362c.chunk.css"
+    "revision": "0c7099a865c9913a6ca9",
+    "url": "/movie-app-react/static/css/main.523de852.chunk.css"
   },
   {
     "revision": "78ae3f71db3cb5c32db6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-app-react/static/js/2.41a12036.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d05999c5b07fa8aaf733",
-    "url": "/movie-app-react/static/js/main.56801802.chunk.js"
+    "revision": "0c7099a865c9913a6ca9",
+    "url": "/movie-app-react/static/js/main.d373a420.chunk.js"
   },
   {
     "revision": "64e0037c206578654b3b",
