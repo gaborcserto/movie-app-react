b37246c9c07a04a5e0413cca1ef213777e305fd6
Gabor Cserto <csertog@gmail.com>
2020-08-04 12:49:54 +0200

    Updates

--- precache-manifest.6b8a95657d8cf829b46493713583014c.js
+++ precache-manifest.6b8a95657d8cf829b46493713583014c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55b081c49865067182625c6854f7466c",
+    "revision": "d4b9185b3da074991062249a9d631b6a",
     "url": "/movie-app-react/index.html"
   },
   {
-    "revision": "030bdb58d477ee830687",
-    "url": "/movie-app-react/static/css/main.db7a4261.chunk.css"
+    "revision": "ab91a6e33ad51775bfa1",
+    "url": "/movie-app-react/static/css/main.538ee0c3.chunk.css"
   },
   {
     "revision": "48a9bd941116ccd754ac",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-app-react/static/js/2.32529567.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "030bdb58d477ee830687",
-    "url": "/movie-app-react/static/js/main.d373a420.chunk.js"
+    "revision": "ab91a6e33ad51775bfa1",
+    "url": "/movie-app-react/static/js/main.d1b97c49.chunk.js"
   },
   {
     "revision": "64e0037c206578654b3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-app-react/precache-manifest.b8bf3cef29dfdf116c890efa15325fde.js"
+  "/movie-app-react/precache-manifest.6b8a95657d8cf829b46493713583014c.js"
 );
 
 self.addEventListener('message', (event) => {
